05c23accf15ec2260dc252ec715a4652d928f7ae
Aaron <aaron.quinn.nfld@gmail.com>
2022-11-17 20:53:17 -0330

    Initial commit, header done

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+const header = () => {
+  return (
+    <header className='cb-header'>
+      <div className='cb-header-text'>
+          Customer Feedback
+      </div>
+    </header>
+  )
+}
+
+export default header
