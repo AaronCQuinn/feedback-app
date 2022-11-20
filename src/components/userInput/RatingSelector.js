9f437ac336de0c73f03b42f6befefa535ed86a4f
Aaron <aaron.quinn.nfld@gmail.com>
2022-11-20 11:09:28 -0330

    Finished submission functionality

--- src/components/userInput/RatingSelector.js
+++ src/components/userInput/RatingSelector.js
@@ -1,18 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import FeedbackContext from '../../context/FeedbackContext'
 import './ratingSelector.css'
 
-const RatingSelector = () => {
+const RatingSelector = ({select}) => {
+    const [selected, setSelected] = useState(10);
+    const {editFeedback} = useContext(FeedbackContext);
+
+    useEffect(() => {
+        setSelected(editFeedback.item.rating);
+    }, [editFeedback]);
+
+    const handleChange = (e) => {
+        setSelected(+e.currentTarget.value);
+        select(+e.currentTarget.value);
+    }
+
     let lowRating = 1;
     let highRating = 10;
     let ratingArray = [];
 
-    const {editFeedback} = useContext(FeedbackContext);
 
     for (let i = lowRating; i <= highRating; i++) {
         ratingArray.push(
             <li className='cb-ratingSelector-li' key={i}>
-                <input type="radio" id={"num" + i} name="rating" value={i} />
+                <input type="radio" id={"num" + i} name="rating" value={i} checked={selected === i} onChange={handleChange}/>
                 <label htmlFor={"num" + i}>{i}</label>
             </li>
         )
--- src/components/userInput/UserInput.js
+++ src/components/userInput/UserInput.js
@@ -1,23 +1,74 @@
-import React from 'react'
+import {useState, useContext} from 'react'
 import RatingSelector from './RatingSelector'
+import {v4 as uuid} from 'uuid'
 import './userInput.css'
+import FeedbackContext from '../../context/FeedbackContext'
 
 const UserInput = () => {
-  return (
-    <div className='cb-userInput-container'>
-        <div className='cb-userInput'>
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(false);
+  const [warningMsg, setWarningMsg] = useState(false);
 
-            <div className='cb-userInput-header'>
-                How would you rate your service with us?
-            </div>
-            <RatingSelector />
-            
-            <div className='cb-userInput-input-group'>
-              <input type="text"/>
-              <input type="submit" className='btn'/>
-            </div>
-        </div>
-    </div>
+  const {addFeedback, editFeedback, updateCurrentFeedback, setEditFeedback, feedback} = useContext(FeedbackContext);
+
+  const handleInputChange = (e) => {
+      // No text, don't show warning.
+      if (text.trim().length <= 10) {
+        setWarningMsg(true);
+        setBtnDisabled(true);
+        // Regular submission conditions.
+      } else {
+        setWarningMsg(false);
+        setBtnDisabled(false);
+      }
+      console.log(feedback);
+      setText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length >= 10) {
+      const newFeedback = {
+        id: uuid(),
+        text,
+        rating,
+      }
+      if (editFeedback.edit) {
+        updateCurrentFeedback(editFeedback.item.id, newFeedback);
+        setEditFeedback({item: {}, edit: false});
+      } else {
+        addFeedback(newFeedback);
+      }
+      setText('');
+    }
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='cb-userInput-container'>
+          <div className='cb-userInput'>
+
+              <div className='cb-userInput-header'>
+                  How would you rate your service with us?
+              </div>
+              
+                <RatingSelector select={(rating => setRating(rating))} />
+                
+                <div className='cb-userInput-input-group'>
+                  <input type="text" placeholder='Leave your review...' onChange={handleInputChange} value={text}/>
+                  <input type="submit" className='btn' disabled={btnDisabled} onSubmit={handleSubmit}/>
+                </div>
+  
+
+              {warningMsg && <div className='cb-userInput-warning'>
+                Please make sure your rating has more then 10 characters and a number is selected!
+              </div>}
+
+          </div>
+      </div>
+    </form>
   )
 }
 
